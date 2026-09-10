0ff782bfc56bc72b9ca0693b41ed18a05556713e
Ilija Pejcic <ilija.pejcic.dev@gmail.com>
2026-09-10 13:28:24 +0200

    Predavanja 10.05 Git Zadatak 1

---
+++ osnove_jave/src/main/java/p10_09_2026/Git_Zadatak1.java
@@ -0,0 +1,11 @@
+package p10_09_2026;
+
+public class Git_Zadatak1 {
+    static void main() {
+        System.out.println("git clone");
+        System.out.println("git status");
+        System.out.println("git add putanja di fajla");
+        System.out.println("git commit -m poruka");
+        System.out.println("git push");
+    }
+}
